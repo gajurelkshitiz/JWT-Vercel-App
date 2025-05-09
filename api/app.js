1b91d55bc297a5c28805ade4a70e18f752c2fddc
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2025-05-09 11:34:40 +0545

    update folder structure

--- api/app.js
+++ api/app.js
@@ -12,9 +12,34 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.static('./public'));
 app.use(express.json());
 
+
 app.use('/api/v1', mainRouter); // mount the main router
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-module.exports = app; // Export the app instance
+
+// connectDB
+const mongoose = require('mongoose');
+const connectDB = require('./db/connect');
+
+
+
+
+
+// define a port
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is listening on port ${process.env.PORT}...`);
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+start();
 
